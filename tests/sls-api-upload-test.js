0e1c407fb0fcd156b992a9d83bd0eef8f85e4629
Alejandro-Gonzalez <ale.g.acosta08@gmail.com>
2019-11-04 18:58:44 -0300

    add api and test

--- tests/sls-api-upload-test.js
+++ tests/sls-api-upload-test.js
@@ -1,21 +1,27 @@
 'use strict';
 
 const assert = require('assert');
-
-/* const AWS = require('aws-sdk-mock');
-
-const sandbox = require('sinon').createSandbox(); */
-
+const APITest = require('@janiscommerce/api-test');
+const globalSandbox = require('sinon').createSandbox();
+const S3 = require('../lib/s3');
 const { SlsApiUpload, SlsApiUploadError } = require('../lib/index');
 
 
-/* const mockS3 = () => {
-	AWS.mock('S3', 'createPresignedPost', (params, callback) => {
-		callback(null, 'successfully put item in database');
-	});
-}; */
+const mockS3 = () => {
+	globalSandbox.stub(S3, 'createPresignedPost').resolves('OK');
+};
 
-const apiExtended = (bucket, path) => {
+const apiExtendedSimple = bucket => {
+	class API extends SlsApiUpload {
+		get bucket() {
+			return bucket;
+		}
+	}
+
+	return API;
+};
+
+const apiExtendedWithGetters = (bucket, path, availableTypes) => {
 	class API extends SlsApiUpload {
 		get bucket() {
 			return bucket;
@@ -24,41 +30,158 @@ const apiExtended = (bucket, path) => {
 		get path() {
 			return path;
 		}
+
+		get availableTypes() {
+			return availableTypes;
+		}
 	}
 
-	return new API();
+	return API;
 };
 
 describe('SlsApiUpload', () => {
-	it('should error if not define bucket', () => {
-		const instance = apiExtended();
-		assert.throws(() => {
-			instance.getPresignedUrlToUpload('file.txt');
+	context('test body request', () => {
+
+		const response = { code: 400 };
+
+		APITest(apiExtendedSimple('test'), [{
+			description: 'should return 400 if request body is missing',
+			response
 		}, {
-			message: 'BUCKET not defined',
-			code: SlsApiUploadError.codes.BUCKET_NOT_DEFINED
-		});
+			description: 'should return 400 if fileName in request body is missing',
+			request: {
+				data: { name: 'test.txt' }
+			},
+			response
+		}, {
+			description: 'should return 400 if add extra data in request body received',
+			request: {
+				data: { fileName: 'test.txt', test: 1 }
+			},
+			response
+		}, {
+			description: 'should return 400 if fileName has invalid type',
+			request: {
+				data: { fileName: 1 }
+			},
+			response
+		}]);
 	});
 
-	it('should error if not define path', () => {
-		const instance = apiExtended('bucket-name');
+	context('test required properties', () => {
 
-		assert.throws(() => {
-			instance.getPresignedUrlToUpload('file.txt');
-		}, {
-			message: 'PATH not defined',
-			code: SlsApiUploadError.codes.PATH_NOT_DEFINED
-		});
+		APITest(apiExtendedSimple(), [{
+			description: 'should return 400 if not define bucket',
+			request: { data: { fileName: 'test.txt' } },
+			response: { code: 400, body: { message: SlsApiUploadError.messages.BUCKET_NOT_DEFINED } }
+		}]);
+
+		APITest(apiExtendedSimple({}), [{
+			description: 'should return 400 if pass invalid bucket',
+			request: { data: { fileName: 'test.txt' } },
+			response: { code: 400, body: { message: SlsApiUploadError.messages.BUCKET_NOT_STRING } }
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', null), [{
+			description: 'should return 400 if pass invalid path',
+			request: { data: { fileName: 'test.txt' } },
+			response: { code: 400, body: { message: SlsApiUploadError.messages.PATH_NOT_STRING } }
+		}]);
+
+		APITest(apiExtendedSimple('bucket-name'), [{
+			description: 'should return 400 if pass fileName with invalid extension',
+			request: {
+				data: { fileName: 'test.extension' }
+			},
+			response: { code: 400, body: { message: SlsApiUploadError.messages.FILE_TYPE_NOT_RECOGNIZED } }
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', 'files', null), [{
+			description: 'should return 400 if pass invalid availableTypes',
+			request: { data: { fileName: 'test.txt' } },
+			response: { code: 400, body: { message: SlsApiUploadError.messages.AVAILABLE_TYPES_NOT_ARRAY } }
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', 'files', ['application/json']), [{
+			description: 'should return 400 if pass filename with extension not available',
+			request: { data: { fileName: 'test.txt' } },
+			response: { code: 400, body: { message: SlsApiUploadError.messages.FILE_TYPE_NOT_AVAILABLE } }
+		}]);
+
 	});
 
-	it('should error if pass fileName', () => {
-		const instance = apiExtended('bucket-name', 'files/images');
+	context('test correct usage', () => {
+		mockS3();
 
-		assert.throws(() => {
-			instance.getPresignedUrlToUpload();
-		}, {
-			message: 'fileName not defined',
-			code: SlsApiUploadError.codes.FILE_NAME_NOT_DEFINED
-		});
+		APITest(apiExtendedSimple('bucket-name'), [{
+			description: 'should return 200 if file txt',
+			request: { data: { fileName: 'test.text' } },
+			response: { code: 200 }
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', 'files', ['application/json']), [{
+			description: 'should return 200 if pass a filename json with availableTypes',
+			request: { data: { fileName: 'test.json' } },
+			response: { code: 200 }
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', 'files/', []), [{
+			before: sandbox => {
+				sandbox.spy(SlsApiUpload.prototype, 'resolvePath');
+			},
+			description: 'should return 200 if pass filename with path correct',
+			request: { data: { fileName: 'test.json' } },
+			response: { code: 200 },
+			after: () => {
+				const call = SlsApiUpload.prototype.resolvePath.getCall(0);
+
+				assert(call.returnValue === 'files/');
+			}
+		}]);
+
+
+		APITest(apiExtendedWithGetters('bucket-name', '/files', []), [{
+			before: sandbox => {
+				sandbox.spy(SlsApiUpload.prototype, 'resolvePath');
+			},
+			description: 'should return 200 if pass filename with path to resolve',
+			request: { data: { fileName: 'test.json' } },
+			response: { code: 200 },
+			after: () => {
+				const call = SlsApiUpload.prototype.resolvePath.getCall(0);
+
+				assert(call.returnValue === 'files/');
+			}
+		}]);
+
+		APITest(apiExtendedWithGetters('bucket-name', 'files', []), [{
+			before: sandbox => {
+				sandbox.spy(SlsApiUpload.prototype, 'resolvePath');
+			},
+			description: 'should return 200 if pass filename with path to resolve',
+			request: { data: { fileName: 'test.json' } },
+			response: { code: 200 },
+			after: () => {
+				const call = SlsApiUpload.prototype.resolvePath.getCall(0);
+
+				assert(call.returnValue === 'files/');
+			}
+		}]);
+
+		APITest(apiExtendedSimple('bucket-name'), [{
+			before: sandbox => {
+				sandbox.spy(SlsApiUpload.prototype, 'resolvePath');
+			},
+			description: 'should return 200 if pass filename with path empty',
+			request: { data: { fileName: 'test.json' } },
+			response: { code: 200 },
+			after: () => {
+				const call = SlsApiUpload.prototype.resolvePath.getCall(0);
+
+				assert(call.returnValue === '');
+			}
+		}]);
+
 	});
+
 });
