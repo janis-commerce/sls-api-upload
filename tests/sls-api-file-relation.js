41799c55fce71885db459353343b837cf0b733d5
Alejandro-Gonzalez <ale.g.acosta08@gmail.com>
2019-12-12 16:34:04 -0300

    add tests for api relation file

--- tests/sls-api-file-relation.js
+++ tests/sls-api-file-relation.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const mime = require('mime');
+const assert = require('assert');
 const APITest = require('@janiscommerce/api-test');
+const Model = require('@janiscommerce/model');
+const S3 = require('@janiscommerce/s3');
 const { SlsApiFileRelation, SlsApiFileRelationError } = require('../lib/index');
 
 
@@ -20,25 +24,23 @@ describe('SlsApiRelation', () => {
 			get bucket() {
 				return bucket;
 			}
-
-			get table() {
-				return table;
-			}
-
-			get databaseKey() {
-				return databaseKey;
-			}
-
-			get customFieldsStruct() {
-				return customFieldsStruct;
-			}
 		}
+
+		const getters = { databaseKey, table, customFieldsStruct };
+
+		Object.keys(getters).forEach(getterName => {
+			if(getters[getterName] !== undefined) {
+				Object.defineProperty(API.prototype, getterName, {
+					get: () => getters[getterName]
+				});
+			}
+		});
 
 		return API;
 	};
 
 
-	context('test validations', () => {
+	context('test validate', () => {
 		APITest(apiExtendedSimple(), [{
 			description: 'should return 400 if entityIdField is not defined',
 			response: { code: 400, body: { message: SlsApiFileRelationError.messages.ENTITY_ID_FIELD_NOT_DEFINED } }
@@ -66,7 +68,8 @@ describe('SlsApiRelation', () => {
 
 		APITest(apiExtendedSimple({
 			entityIdField: 'test',
-			bucket: 'test'
+			bucket: 'test',
+			table: null
 		}), [{
 			description: 'should return 400 if table is not a string',
 			response: { code: 400, body: { message: SlsApiFileRelationError.messages.TABLE_NOT_DEFINED } }
@@ -174,16 +177,200 @@ describe('SlsApiRelation', () => {
 		}]);
 	});
 
+	it('test buildModel method', () => {
+		const ModelBuilded = SlsApiFileRelation.buildModel('table_test', 'database_test');
+		const modelInstance = new ModelBuilded();
+
+		assert(modelInstance.table === 'table_test');
+		assert(modelInstance.databaseKey === 'database_test');
+	});
+
 	context('test process', () => {
 		APITest(apiExtendedSimple({
 			entityIdField: 'test',
-			bucket: 'test',
-			table: 'test',
-			customFieldsStruct: { type: 'string' }
+			bucket: 'test'
 		}), [{
-			description: 'should return 400 if pass incorrect custom fields in body',
-			request: { data: { fileName: 'test.js', fileSource: 'files/test.js', type: 132 } },
-			response: { code: 400 }
+			before: sandbox => {
+				sandbox.stub(S3, 'headObject').rejects();
+				sandbox.stub(Model.prototype, 'insert');
+			},
+			session: true,
+			description: 'should return 500 if fail headObject',
+			request: {
+				data: { fileName: 'test.png', fileSource: 'files/test.png' },
+				pathParameters: [1]
+			},
+			response: { code: 500 },
+			after: (afterResponse, sandbox) => {
+				sandbox.assert.called(S3.headObject);
+				sandbox.assert.notCalled(Model.prototype.insert);
+			}
 		}]);
+
+
+		APITest(apiExtendedSimple({
+			entityIdField: 'test',
+			bucket: 'test'
+		}), [{
+			before: sandbox => {
+				sandbox.stub(S3, 'headObject').resolves({
+					ContentType: 'image/png',
+					ContentLength: 10000
+				});
+				sandbox.stub(Model.prototype, 'insert').rejects();
+			},
+			session: true,
+			description: 'should return 500 if fail model insert',
+			request: {
+				data: { fileName: 'test.png', fileSource: 'files/test.png' },
+				pathParameters: [1]
+			},
+			response: { code: 500 },
+			after: (afterResponse, sandbox) => {
+				sandbox.assert.called(Model.prototype.insert);
+				sandbox.assert.called(S3.headObject);
+			}
+		}]);
+
+		APITest(apiExtendedSimple({
+			entityIdField: 'test',
+			bucket: 'test'
+		}), [{
+			before: sandbox => {
+				sandbox.stub(S3, 'headObject').resolves({
+					ContentType: 'image/png',
+					ContentLength: 10000
+				});
+				sandbox.stub(Model.prototype, 'insert').resolves(12345);
+			},
+			session: true,
+			description: 'should return 200 with valid data',
+			request: {
+				data: { fileName: 'test.png', fileSource: 'files/test.png' },
+				pathParameters: [1]
+			},
+			response: { code: 201, body: { id: 12345 } },
+			after: (afterResponse, sandbox) => {
+				sandbox.assert.calledWithExactly(Model.prototype.insert, {
+					test: 1,
+					path: 'files/test.png',
+					name: 'test.png',
+					mimeType: 'image/png',
+					type: 'image',
+					size: 10000
+				});
+			}
+		}]);
+
+		APITest(apiExtendedSimple({
+			entityIdField: 'test',
+			bucket: 'test',
+			customFieldsStruct: {
+				description: 'string',
+				order: 'number'
+			}
+		}), [{
+			before: sandbox => {
+				sandbox.stub(S3, 'headObject').resolves({
+					ContentType: 'image/png',
+					ContentLength: 10000
+				});
+				sandbox.stub(Model.prototype, 'insert').resolves(12345);
+			},
+			session: true,
+			description: 'should return 200 with valid data and custom fields struct',
+			request: {
+				data: {
+					fileName: 'test.png',
+					fileSource: 'files/test.png',
+					description: 'test description',
+					order: 1
+				},
+				pathParameters: [1]
+			},
+			response: { code: 201, body: { id: 12345 } },
+			after: (afterResponse, sandbox) => {
+				sandbox.assert.calledWithExactly(Model.prototype.insert, {
+					test: 1,
+					path: 'files/test.png',
+					name: 'test.png',
+					mimeType: 'image/png',
+					type: 'image',
+					size: 10000,
+					description: 'test description',
+					order: 1
+				});
+			}
+		}]);
+
+		const types = [{
+			extension: '.jpg',
+			type: 'image'
+		}, {
+			extension: '.png',
+			type: 'image'
+		}, {
+			extension: '.jpeg',
+			type: 'image'
+		}, {
+			extension: '.doc',
+			type: 'doc'
+		}, {
+			extension: '.docx',
+			type: 'doc'
+		}, {
+			extension: '.mp4',
+			type: 'video'
+		}, {
+			extension: '.flv',
+			type: 'video'
+		}, {
+			extension: '.mp3',
+			type: 'audio'
+		}, {
+			extension: '.wav',
+			type: 'audio'
+		}, {
+			extension: '.ods',
+			type: 'sheet'
+		}, {
+			extension: '.csv',
+			type: 'sheet'
+		}, {
+			extension: '.xlsx',
+			type: 'sheet'
+		}, {
+			extension: '.json',
+			type: 'other'
+		}, {
+			extension: '.txt',
+			type: 'other'
+		}];
+
+		APITest(apiExtendedSimple({
+			entityIdField: 'test',
+			bucket: 'test'
+		}),
+		types.map(({ extension, type }) => ({
+			before: sandbox => {
+				sandbox.stub(S3, 'headObject').resolves({
+					ContentType: mime.getType(extension),
+					ContentLength: 10000
+				});
+				sandbox.stub(Model.prototype, 'insert').resolves(12345);
+			},
+			session: true,
+			description: 'should return 200 with valid data with image type',
+			request: {
+				data: { fileName: `test${extension}`, fileSource: `files/test${extension}` },
+				pathParameters: [1]
+			},
+			response: { code: 201, body: { id: 12345 } },
+			after: (afterResponse, sandbox) => {
+				sandbox.assert.calledWithExactly(Model.prototype.insert, sandbox.match({
+					type
+				}));
+			}
+		})));
 	});
 });
